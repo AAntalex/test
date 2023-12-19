f93e014b286dddd41acac7d92e3506b1177ec74d
aantalex <aantalex@gmail.com>
2023-12-19 21:51:36 +0300

    master

--- src/test/java/ApplicationTests.java
+++ src/test/java/ApplicationTests.java
@@ -1,8 +1,12 @@
+import com.antalex.db.service.SequenceGenerator;
 import com.antalex.db.service.ShardEntityManager;
+import com.antalex.db.service.impl.ApplicationSequenceGenerator;
+import com.antalex.db.service.impl.ApplicationSequenceGenerator2;
 import com.antalex.db.service.impl.ShardDatabaseManager;
 import com.antalex.domain.persistence.repository.AdditionalParameterRepository;
 import com.antalex.optimizer.OptimizerApplication;
 import com.antalex.domain.persistence.entity.AdditionalParameterEntity;
+import com.antalex.profiler.model.TimeCounter;
 import com.antalex.profiler.service.ProfilerService;
 import com.antalex.service.AdditionalParameterService;
 import liquibase.precondition.core.PreconditionContainer;
@@ -12,7 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
 import java.util.List;
+import java.util.Objects;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OptimizerApplication.class)
@@ -35,7 +44,22 @@ public class ApplicationTests {
 
 	@Test
 	public void ins() {
-        profiler.start("Тест");
+
+
+		SequenceGenerator sequenceGenerator = new ApplicationSequenceGenerator("SEQ_ID", databaseManager.getDataSource());
+		((ApplicationSequenceGenerator) sequenceGenerator).setCacheSize(10000);
+
+		profiler.start("Тест0");
+
+		for (int i = 0; i < 100000; i++) {
+			sequenceGenerator.nextValue();
+		}
+
+		profiler.stop();
+		System.out.println(profiler.printTimeCounter());
+
+
+		profiler.start("Тест");
 
 		List<AdditionalParameterEntity> additionalParameterEntities
 				= additionalParameterService.generate(10000, "TEST", "С_CODE2", "VALUE2");
