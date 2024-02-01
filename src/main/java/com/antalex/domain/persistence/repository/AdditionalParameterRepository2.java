6a68116a0cb16c2f794525f1503022798b5f07a2
AAntalex <aantalex@gmail.com>
2024-02-01 19:54:31 +0300

    test

--- src/main/java/com/antalex/domain/persistence/repository/AdditionalParameterRepository2.java
+++ src/main/java/com/antalex/domain/persistence/repository/AdditionalParameterRepository2.java
@@ -1,11 +1,13 @@
 package com.antalex.domain.persistence.repository;
 
 import com.antalex.db.model.Cluster;
-import com.antalex.db.model.StorageAttributes;
 
+import com.antalex.db.model.StorageAttributes;
 import com.antalex.db.model.enums.ShardType;
 import com.antalex.db.service.ShardDataBaseManager;
+import com.antalex.db.service.ShardEntityManager;
 import com.antalex.db.service.ShardEntityRepository;
+import com.antalex.db.utils.ShardUtils;
 import com.antalex.domain.persistence.entity.AdditionalParameterEntity2;
 import org.springframework.stereotype.Repository;
 
@@ -19,92 +21,77 @@ public class AdditionalParameterRepository2 implements ShardEntityRepository<Add
     private static final String INS_QUERY = "INSERT INTO IBS.Z#VND_ADD_PARAMS (ID, SHARD_VALUE, C_PARENT_ID, C_CODE, C_VALUE) VALUES (?, ?, ?, ?, ?)";
 
     private final ShardDataBaseManager dataBaseManager;
+    private final ShardEntityManager entityManager;
     private final Cluster cluster;
 
-    AdditionalParameterRepository2(ShardDataBaseManager dataBaseManager) {
+    AdditionalParameterRepository2(ShardDataBaseManager dataBaseManager,
+                                   ShardEntityManager entityManager)
+    {
         this.dataBaseManager = dataBaseManager;
+        this.entityManager = entityManager;
         this.cluster = dataBaseManager.getCluster(String.valueOf("DEFAULT"));
     }
 
     @Override
     public AdditionalParameterEntity2 save(AdditionalParameterEntity2 entity) {
         if (Objects.isNull(entity.getId())) {
-            StorageAttributes storageAttributes = new StorageAttributes();
-            storageAttributes.setStored(false);
-            storageAttributes.setCluster(this.cluster);
-            entity.setStorageAttributes(storageAttributes);
-
-
-            entity.setId(dataBaseManager.generateId(storageAttributes));
-        }
-        return entity;
-    }
-
-    public void setStorage(AdditionalParameterEntity2 entity, StorageAttributes storage) {
-        if (Objects.isNull(storage) && Objects.isNull(entity.getStorageAttributes())) {
-            storage = new StorageAttributes();
-            storage.setStored(false);
-            storage.setCluster(this.cluster);
-            entity.setStorageAttributes(storage);
-        } else {
-            if (Objects.isNull(entity.getStorageAttributes())) {
-                entity.setStorageAttributes(storage);
-            } else {
-
-            }
-        }
-
-
-
-
-        if (Objects.isNull(storage) ||
-                !Optional.ofNullable(entity.getStorageAttributes())
-                        .map(StorageAttributes::getCluster)
-                        .orElse(this.cluster)
-                        .getId()
-                        .equals(storage.getCluster().getId()))
-        {
-            if (Objects.isNull(entity.getStorageAttributes())) {
-                storage = new StorageAttributes();
-                storage.setStored(false);
-                storage.setCluster(this.cluster);
-                entity.setStorageAttributes(storage);
-            }
-        } else {
-            if (
-                    Optional.ofNullable(entity.getStorageAttributes())
-                            .map(StorageAttributes::getCluster)
-                            .orElse(this.cluster)
-                            .getId().equals(storage.getCluster().getId()))
-            {
-                if (Objects.isNull(entity.getStorageAttributes())) {
-                    entity.setStorageAttributes(storage);
-                } else {
-                    if (storage.equals(entity.getStorageAttributes())) {
-
-                    }
-                }
-            }
-        }
-
-    }
-
-    public AdditionalParameterEntity2 save(AdditionalParameterEntity2 entity, StorageAttributes storageAttributes) {
-        if (Objects.isNull(entity.getId())) {
-            storageAttributes = new StorageAttributes();
-            storageAttributes.setStored(false);
-            storageAttributes.setCluster(this.cluster);
-            entity.setStorageAttributes(storageAttributes);
-
-
-            entity.setId(dataBaseManager.generateId(storageAttributes));
+            this.setStorage(entity, null);
+            entity.setId(dataBaseManager.generateId(entity));
         }
         return entity;
     }
 
     @Override
+    public Cluster getCluster(AdditionalParameterEntity2 entity) {
+        return cluster;
+    }
+
+    @Override
     public ShardType getShardType(AdditionalParameterEntity2 entity) {
         return SHARD_TYPE;
+    }
+
+    @Override
+    public void setStorage(AdditionalParameterEntity2 entity, final StorageAttributes storage) {
+        if (Objects.isNull(entity.getStorageAttributes())) {
+            entity.setStorageAttributes(
+                    Optional.ofNullable(storage)
+                            .filter(it ->
+                                    this.cluster.getId()
+                                            .equals(it.getCluster().getId())
+                            )
+                            .orElse(
+                                    StorageAttributes.builder()
+                                            .stored(false)
+                                            .cluster(this.cluster)
+                                            .build()
+                            )
+            );
+
+
+            entityManager.setStorage(entity, entity.getStorageAttributes());
+
+
+        } else {
+            if (Objects.nonNull(storage) &&
+                    entity.getStorageAttributes() != storage &&
+                    SHARD_TYPE != ShardType.REPLICABLE &&
+                    this.cluster.getId().equals(storage.getCluster().getId()) &&
+                    Objects.nonNull(entity.getStorageAttributes().getShard()))
+            {
+                if (Objects.isNull(storage.getShard())) {
+                    storage.setShard(entity.getStorageAttributes().getShard());
+                    storage.setShardValue(entity.getStorageAttributes().getShardValue());
+                } else {
+                    storage.setShardValue(
+                            ShardUtils.addShardValue(
+                                    storage.getShardValue(),
+                                    entity.getStorageAttributes().getShardValue()
+                            )
+                    );
+                }
+            }
+        }
     }
 
     public void flush() {
