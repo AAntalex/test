2ce985f03427df828b6ee4cfc08e29b674ddad6a
aantalex <aantalex@gmail.com>
2024-02-02 22:58:12 +0300

    master

--- src/main/java/com/antalex/domain/persistence/repository/AdditionalParameterRepository2.java
+++ src/main/java/com/antalex/domain/persistence/repository/AdditionalParameterRepository2.java
@@ -2,17 +2,14 @@ package com.antalex.domain.persistence.repository;
 
 import com.antalex.db.model.Cluster;
 
-import com.antalex.db.model.StorageAttributes;
 import com.antalex.db.model.enums.ShardType;
 import com.antalex.db.service.ShardDataBaseManager;
 import com.antalex.db.service.ShardEntityManager;
 import com.antalex.db.service.ShardEntityRepository;
-import com.antalex.db.utils.ShardUtils;
 import com.antalex.domain.persistence.entity.AdditionalParameterEntity2;
 import org.springframework.stereotype.Repository;
 
 import java.util.Objects;
-import java.util.Optional;
 
 
 @Repository
@@ -24,21 +21,21 @@ public class AdditionalParameterRepository2 implements ShardEntityRepository<Add
     private final ShardEntityManager entityManager;
     private final Cluster cluster;
 
+    @Override
+    public AdditionalParameterEntity2 save(AdditionalParameterEntity2 entity) {
+        if (Objects.isNull(entity.getId())) {
+            entityManager.setStorage(entity, null);
+            entity.setId(dataBaseManager.generateId(entity));
+        }
+        return entity;
+    }
+
     AdditionalParameterRepository2(ShardDataBaseManager dataBaseManager,
                                    ShardEntityManager entityManager)
     {
         this.dataBaseManager = dataBaseManager;
         this.entityManager = entityManager;
         this.cluster = dataBaseManager.getCluster(String.valueOf("DEFAULT"));
-    }
-
-    @Override
-    public AdditionalParameterEntity2 save(AdditionalParameterEntity2 entity) {
-        if (Objects.isNull(entity.getId())) {
-            this.setStorage(entity, null);
-            entity.setId(dataBaseManager.generateId(entity));
-        }
-        return entity;
     }
 
     @Override
@@ -52,46 +49,8 @@ public class AdditionalParameterRepository2 implements ShardEntityRepository<Add
     }
 
     @Override
-    public void setStorage(AdditionalParameterEntity2 entity, final StorageAttributes storage) {
-        if (Objects.isNull(entity.getStorageAttributes())) {
-            entity.setStorageAttributes(
-                    Optional.ofNullable(storage)
-                            .filter(it ->
-                                    this.cluster.getId()
-                                            .equals(it.getCluster().getId())
-                            )
-                            .orElse(
-                                    StorageAttributes.builder()
-                                            .stored(false)
-                                            .cluster(this.cluster)
-                                            .build()
-                            )
-            );
+    public void setDependentStorage(AdditionalParameterEntity2 entity) {
 
-
-            entityManager.setStorage(entity, entity.getStorageAttributes());
-
-
-        } else {
-            if (Objects.nonNull(storage) &&
-                    entity.getStorageAttributes() != storage &&
-                    SHARD_TYPE != ShardType.REPLICABLE &&
-                    this.cluster.getId().equals(storage.getCluster().getId()) &&
-                    Objects.nonNull(entity.getStorageAttributes().getShard()))
-            {
-                if (Objects.isNull(storage.getShard())) {
-                    storage.setShard(entity.getStorageAttributes().getShard());
-                    storage.setShardValue(entity.getStorageAttributes().getShardValue());
-                } else {
-                    storage.setShardValue(
-                            ShardUtils.addShardValue(
-                                    storage.getShardValue(),
-                                    entity.getStorageAttributes().getShardValue()
-                            )
-                    );
-                }
-            }
-        }
     }
 
     public void flush() {
