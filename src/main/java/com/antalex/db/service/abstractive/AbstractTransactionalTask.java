d374f099d1cb4692d693490355d4d10410781f30
AAntalex <aantalex@gmail.com>
2024-03-14 19:40:43 +0300

    master

--- src/main/java/com/antalex/db/service/abstractive/AbstractTransactionalTask.java
+++ src/main/java/com/antalex/db/service/abstractive/AbstractTransactionalTask.java
@@ -6,7 +6,6 @@ import com.antalex.db.model.enums.QueryType;
 import com.antalex.db.model.enums.TaskStatus;
 import com.antalex.db.service.api.TransactionalQuery;
 import com.antalex.db.service.api.TransactionalTask;
-import com.antalex.db.service.impl.TransactionalSQLQuery;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
