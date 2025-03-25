efae1cb51411279496a86b1b625883fedbc66feb
aantalex <aantalex@gmail.com>
2025-03-25 18:56:26 +0300

    master

--- src/main/java/com/antalex/domain/persistence/criteria/MainDocumCriteria.java
+++ src/main/java/com/antalex/domain/persistence/criteria/MainDocumCriteria.java
@@ -3,12 +3,10 @@ package com.antalex.domain.persistence.criteria;
 import com.antalex.db.annotation.Criteria;
 import com.antalex.db.annotation.CriteriaAttribute;
 import com.antalex.db.annotation.Join;
-import com.antalex.domain.persistence.entity.shard.app.Account;
-import com.antalex.domain.persistence.entity.shard.app.Client;
-import com.antalex.domain.persistence.entity.shard.app.MainDocum;
-import com.antalex.domain.persistence.entity.shard.app.ClientCategory;
+import com.antalex.domain.persistence.entity.shard.app.*;
 import lombok.Data;
 
+import javax.persistence.criteria.JoinType;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -20,6 +18,12 @@ import java.util.Date;
         joins = {
                 @Join(from = Account.class, alias = "acc_dt", on = "${acc_dt} = ${md.accDt}"),
                 @Join(from = Account.class, alias = "acc_ct", on = "${id} = ${md.accCt}"),
+                @Join(
+                        from = ExternalPayment.class,
+                        alias = "ext_doc",
+                        on = "${doc} = ${md.id}",
+                        joinType = JoinType.LEFT
+                ),
                 @Join(from = Client.class, alias = "cl_ct", on = "${id} = ${acc_ct.client}"),
                 @Join(from = Client.class, alias = "cl_dt", on = "${id} = ${acc_dt.client}"),
                 @Join(from = ClientCategory.class, alias = "cl_cat", on = "${id} = ${cl_ct.category}"),
@@ -41,4 +45,7 @@ public class MainDocumCriteria {
     private String dtClientName;
     @CriteriaAttribute("cl_ct.name")
     private String ctClientName;
+    @CriteriaAttribute("ext_doc.receiver")
+    private String receiver;
+
 }
